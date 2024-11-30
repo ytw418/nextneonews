ea6f32b08e53fcc8f01a744fdb8e43b3dc3fae81
ytw418 <ytw418@gmail.com>
2024-12-01 04:37:28 +0900

    fix: 상세페이지 오류 수정

--- app/(main)/post/[id]/page.tsx
+++ app/(main)/post/[id]/page.tsx
@@ -1,12 +1,11 @@
 import PostClient from "./PostClient";
-import { NewsCardProps } from "@/components/common/NewsCard";
 
-export default async function PostPage({ params }: { params: { id: string } }) {
-  try {
-    // const post = await getPost(params.id);
-    return <PostClient />;
-  } catch (error) {
-    // console.error("Post page error:", error);
-    return <div>포스트를 불러오는 중 오류가 발생했습니다.</div>;
-  }
-}
+const Page = () => {
+  return (
+    <div>
+      <PostClient />;
+    </div>
+  );
+};
+
+export default Page;
